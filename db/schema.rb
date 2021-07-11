10759c05ff5f78a9a3d08f09854dcd287efa3449
Glêsio Santos <glesioss@gmail.com>
2021-07-11 18:34:19 -0300

    created model and migration to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_210223) do
+ActiveRecord::Schema.define(version: 2021_07_11_212712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "title"
+    t.string "location"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end

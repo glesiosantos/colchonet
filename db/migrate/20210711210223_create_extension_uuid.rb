6315c65555022fe33295000324bac67675942d4f
Glêsio Santos <glesioss@gmail.com>
2021-07-11 19:07:18 -0300

    add user controller

--- db/migrate/20210711210223_create_extension_uuid.rb
+++
@@ -1,5 +0,0 @@
-class CreateExtensionUuid < ActiveRecord::Migration[6.1]
-  def execute
-    "CREATE EXTENSION IF NOT EXISTS 'uuid-ossp'"
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_212712) do
+ActiveRecord::Schema.define(version: 2021_07_11_213059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 2021_07_11_212712) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "fullname"
+    t.string "email"
+    t.string "password"
+    t.string "location"
+    t.text "bio"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
